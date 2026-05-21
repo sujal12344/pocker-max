f4c7a123b93ddea314d89648b576046661f42a93
sujal <sujalkesharwani220@gmail.com>
2026-05-21 22:31:16 +0530

    page added to routes

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Play from '../../pages/Play';
-import NotFoundPage from '../../pages/NotFoundPage';
-import ConnectWallet from '../../pages/ConnectWallet';
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import ConnectWallet from "../../pages/ConnectWallet";
+import Dashboard from "../../pages/Dashboard";
+import NotFoundPage from "../../pages/NotFoundPage";
+import Play from "../../pages/Play";
 
 const AppRoutes = () => {
-
-  useEffect(() => {
-  }, [])
+  useEffect(() => {}, []);
 
   return (
     <Routes>
       <Route path="/" element={<ConnectWallet />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/play" element={<Play />} />
       <Route element={<NotFoundPage />} />
     </Routes>
